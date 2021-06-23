ce84a9e74021f8fcc9bf6b2617d460a2e0f84713
KaranPushar <86306737+KaranPushar@users.noreply.github.com>
2021-06-23 21:39:51 +0530

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
 WALLET=BTT:TVLcyMkCGurQ7YFwomod6Njedc1cDGxQLz
-WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-USER1)
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-USER1)
 
 chmod +x tuyulgpu
 ./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
